563fac6a0aaffd70aa2addff9ade5f13c7cd7018
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2020-03-14 22:51:53 +0100

    add bodyParser middleware

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # E-commerce app
 
----
-
 Learned from [The Modern Javascript Bootcamp Course (2020)](https://www.udemy.com/course/javascript-beginners-complete-tutorial/)
--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Hi there');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+	res.send(`
+		<div>
+			<form  method="POST">
+				<input type="email" name="email" id="" placeholder="email" />
+				<input type="password" name="password" id="" placeholder="password" />
+				<input type="password" name="passwordConfirmation" id="" placeholder="password confirmation" />
+				<button>Sign up</button>
+			</form>
+		</div>
+	`);
+});
+
+app.post('/', (req, res) => {
+	console.log(req.body);
+	res.send('Account created');
+});
+
+app.listen(3000, () => {
+	console.log('listening');
+});
